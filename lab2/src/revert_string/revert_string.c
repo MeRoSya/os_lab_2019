360e4ca6eb78e1a891af58f150c6f3aba670b67e
merosya <Zhuga200117@gmail.com>
2021-02-17 14:15:12 +0000

    Выполнены первые 2 задания

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,14 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	char temp;
+	int n = strlen(str);
+	for(int i=0; i < n/2; i++){
+		temp = *(str+i);
+		*(str+i) = *(str+n-1-i);
+		*(str+n-1-i) = temp;
+	}
 }
 
